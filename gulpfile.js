da259cd12383ff2c7711ac0200f1b7d61c19ae69
DydFreeman <riccardo.pancotti@gmail.com>
2017-07-23 16:34:23 +0200

    - Add gulpTask for minor releases  - Modified node version used on travis  - Reset app version on package.json

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+  bump = require('gulp-bump'),
+  git = require('gulp-git'),
+  inject = require('gulp-inject-string'),
+  merge = require('merge-stream'),
+  tag_version = require('gulp-tag-version'),
+  tslint = require('gulp-tslint'),
+  typings = require('gulp-typings'),
+  shell = require('gulp-shell');
+
+var paths = {
+  src_ts: 'src/**/*.ts',
+  tests_ts: 'test/**/*.ts',
+};
+
+function versionBump(semver) {
+  return gulp
+    .src(['./package.json'])
+    .pipe(bump({ type: semver }))
+    .pipe(gulp.dest('./'))
+    .pipe(git.commit('bump package version'))
+    .pipe(tag_version());
+}
+
+gulp.task('compile', shell.task(['npm run vscode:prepublish']));
+
+gulp.task('major', ['compile'], function() {
+  return versionBump('major');
+});
+
+gulp.task('minor', ['compile'], function() {
+  return versionBump('minor');
+});
