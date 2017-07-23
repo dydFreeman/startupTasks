6950dbda55702b963eb6b87c6e6b762ab64200c7
DydFreeman <riccardo.pancotti@gmail.com>
2017-07-23 12:26:22 +0200

    Alpha version of startupTasks extension.

---
+++ src/extension.ts
@@ -0,0 +1,18 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    let taskList: Array<any> = vscode.workspace.getConfiguration('startuptask').get('taskList');
+
+    taskList.forEach((item, index) => {
+        if (item.command && item.arg) {
+            let term = vscode.window.createTerminal("startupTask_" + index);
+            let strCmd = "node_modules\\.bin\\" + item.command + ".cmd" + " " + item.arg;
+            term.sendText(strCmd);
+        }
+    });
+}
+
+export function deactivate() {
+}
