73ed29eb1928feda0ee41fb0efa9a3d4db78319b
DydFreeman <riccardo.pancotti@gmail.com>
2017-07-23 17:12:14 +0200

    Fixed typo on field name ('args', not 'arg')

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
     let taskList: Array<any> = vscode.workspace.getConfiguration('startuptask').get('taskList');
 
     taskList.forEach((item, index) => {
-        if (item.command && item.arg) {
+        if (item.command && item.args) {
             let term = vscode.window.createTerminal("startupTask_" + index);
             let strCmd = "node_modules\\.bin\\" + item.command + ".cmd" + " " + item.arg;
             term.sendText(strCmd);
